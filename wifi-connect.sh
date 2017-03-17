bc3d1acdd87770f3f22428ac88167c03042e0ac6
craptalk <craptalk8@gmail.com>
2017-03-17 20:59:53 +0700

    wifi-connect modified(2)

--- wifi-connect.sh
+++ wifi-connect.sh
@@ -37,14 +37,23 @@ case $answer in
 	2)echo -n "Enter SSID:"
 		  read ssid
           echo -n "Enter passphrase:"
-		  read pass
+		  read -s pass
+	  while [[ -z $ssid || -z $pass ]];
+	  do
+		echo -e "\n\n\n\n${BRed}Please enter SSID and Password!!!\n\n"
+	   	echo -n "Enter SSID:"
+		read ssid
+		echo -e "\n\n"
+		echo -n "Enter passphrase:"
+		read -s pass
+	  done
 	  echo -en "${BGreen}Connecting to $ssid.....\n"
 	  nmcli device wifi connect $ssid password $pass 2>/dev/null
 	  if [ $? -eq 0 ]; 
 	  then
     		echo -en "${Blue}Connected!!\n\n\n"
 	  else
-		echo -en "${BRed}Failed to connect!!!"
+		echo -en "${BRed}Failed to connect\n\n\n!!!"
   	  fi;;
 	3)if [ $? -eq 0 ];
 	  then echo "Exiting...."
